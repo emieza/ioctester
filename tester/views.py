90e9af7dadddf177d8b9dc25892a0e4e5e56bce3
Enric <emieza@xtec.cat>
2024-09-05 22:27:11 +0200

    instruccions amb connexió ssh

--- tester/views.py
+++ tester/views.py
@@ -6,51 +6,57 @@ import subprocess, os
 from .models import *
 
 def index(request):
-	proves = Prova.objects.all()
-	return render( request, "index.html", {"proves":proves} )
+    proves = Prova.objects.all()
+    return render( request, "index.html", {"proves":proves} )
 
 
 @login_required
 def executa_prova(request,prova_id):
-	ip = get_client_ip(request)
-	resultat = "Iniciant prova {} per a usuari {} en IP={}\n".format(
-					prova_id, request.user, ip)
-	try:
-		prova = Prova.objects.get(id=prova_id)
-		intent = Intent( prova=prova, alumne=request.user,
-							ip=ip, registre=resultat )
-		instruccio = prova.instruccio.replace("%IP",ip)
-		resultat += "Executant instrucció:\n" + instruccio + "\n"
-		intent.save()
+    ip = get_client_ip(request)
+    resultat = "Iniciant prova {} per a usuari {} en IP={}\n".format(
+                    prova_id, request.user, ip)
+    try:
+        prova = Prova.objects.get(id=prova_id)
+        intent = Intent( prova=prova, alumne=request.user,
+                            ip=ip, registre=resultat )
+        resultat += "PROVA: {}\n".format(prova.nom)
+        instruccio = prova.instruccio.replace("%IP",ip)
+        resultat += "Executant instrucció: " + instruccio + "\n"
+        intent.save()
 
-		# Executar comanda
-		#if not prova.connexio_ssh:
-		completedProc = subprocess.run( instruccio, shell=True,
-							capture_output=True )
+        # Afegim connexio SSH si pertoca        
+        if prova.connexio_ssh:
+            conn_str = "ssh isard@" + ip
+            instruccio = conn_str + " " + instruccio
 
-		print(str(completedProc))
+        # Executar comanda
+        completedProc = subprocess.run( instruccio, shell=True,
+                            capture_output=True )
+        #print(str(completedProc))
 
-		if completedProc.returncode==0:
-			resultat +="[SUCCESS]\n{}\n".format(completedProc.stdout.decode("utf-8"))
-		else:
-			resultat += "[FAIL] code={}\n\t{}\n".format(
-				completedProc.returncode, completedProc.stderr.decode("utf-8") )
+        if completedProc.returncode==0:
+            resultat +="[SUCCESS]\n{}\n[SUCCESS]\n".format(completedProc.stdout.decode("utf-8"))
+        else:
+            resultat += "[FAIL] exit code = {}\n\t{}\n[FAIL]\n".format(
+                completedProc.returncode, completedProc.stderr.decode("utf-8") )
+            if prova.connexio_ssh and completedProc.returncode==255:
+                resultat += "\tProbablement la teva màquina no es deixa accedir per SSH. Comprova que no hagis malmès l'usuari de connexió (isard)."
 
-		# Guardem a la BD
-		intent.registre = resultat
-		intent.save()
+        # Guardem a la BD
+        intent.registre = resultat
+        intent.save()
 
-		# Retornar resultats
-		ret = {
-			"status": "OK",
-			"message": resultat
-		}
-		return JsonResponse(ret)
-	except Exception as e:
-		return JsonResponse({
-			"status":"ERROR",
-			"message":resultat + repr(e)
-		})
+        # Retornar resultats
+        ret = {
+            "status": "OK",
+            "message": resultat
+        }
+        return JsonResponse(ret)
+    except Exception as e:
+        return JsonResponse({
+            "status":"ERROR",
+            "message":resultat + repr(e)
+        })
 
 
 def get_client_ip(request):
